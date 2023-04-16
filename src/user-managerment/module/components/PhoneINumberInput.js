38a7b24353c922b6ac1f08d04cc4461515ab7006
Bravo Stack <chukwuebukaonuh23@gmail.com>
2023-04-16 22:24:38 +0100

    refactored the PhoneNumberInput Component

--- src/user-managerment/module/components/PhoneINumberInput.js
+++ src/user-managerment/module/components/PhoneINumberInput.js
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import React from 'react'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 import "./phonenumber.scss"
 
 const PhoneINumberInput = ({
     onChange,
+    name,
     value,
     country='ng',
-    autoFormat=true
+    autoFormat=true,
+    placeholder="(+234) 999 999 9999"
 }) => {
 
     return (
         <PhoneInput
         country={country}
+        countryCodeEditable={false}
         autoFormat={autoFormat}
+        placeholder={placeholder}
         value={value}
         onChange={onChange} />
     )
