2fdc9d99728becea97394af49f95946956aab7cd
Bravo Stack <chukwuebukaonuh23@gmail.com>
2023-04-26 21:30:58 +0100

    Created an empty state component for the transaction list

--- src/user-managerment/module/components/Dashboard/Portfolio/TransactionList.js
+++ src/user-managerment/module/components/Dashboard/Portfolio/TransactionList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TransactionList = ({transactionData}) => {
     return (
-        <div className='flex flex-col gap-[2px] pb-5'>
+        <div className='flex flex-col h-full w-full gap-[2px] pb-5'>
             {
                 transactionData && transactionData?.length!==0?
                 transactionData.map((transactData, index)=>(
@@ -57,7 +57,17 @@ const TransactionList = ({transactionData}) => {
                         </div>
                     </div>
                 )):
-                <></>
+                <div className='flex h-full w-full items-center gap-8 justify-center flex-col mt-20'>
+
+                    <img
+                    alt=""
+                    className='w-[120px]'
+                    src='/images/dashboard/empty-transaction.png' />
+
+                        <h3 className='font-normal text-sm text-[#645B75] w-full text-center'>
+                        No record found.
+                    </h3>
+                </div>
             }
         </div>
     )
