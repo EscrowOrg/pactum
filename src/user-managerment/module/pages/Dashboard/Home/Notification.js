27033b7d306d0c5b790fb1447caf864d0998b4b1
Ebuka Nwachukwu <ebukanwachukwu885@gmail.com>
2023-06-15 17:06:39 +0100

    changes ""

--- src/user-managerment/module/pages/Dashboard/Home/Notification.js
+++ src/user-managerment/module/pages/Dashboard/Home/Notification.js
@@ -10,21 +10,21 @@ const Notification = () => {
 
     })
 
-const handleMessage =()=>{
-    messaging.requestPermission().then(() => {
-    console.log('Notification permission granted.');
-    // Get the device token
-    return messaging.getToken();
-  }).then((token) => {
-    console.log('Device token:', token);
-    // Send the token to your server to associate it with the user
-     axios.post(`${VALIDATE_DEVICE_TOKEN}`, token)
+// const handleMessage =()=>{
+//     messaging.requestPermission().then(() => {
+//     console.log('Notification permission granted.');
+//     // Get the device token
+//     return messaging.getToken();
+//   }).then((token) => {
+//     console.log('Device token:', token);
+//     // Send the token to your server to associate it with the user
+//      axios.post(`${VALIDATE_DEVICE_TOKEN}`, token)
     
-  }).catch((error) => {
-    console.error('Error obtaining permission:', error);
-  });
+//   }).catch((error) => {
+//     console.error('Error obtaining permission:', error);
+//   });
 
-}
+// }
     return (
         <PageWrapper>
 
